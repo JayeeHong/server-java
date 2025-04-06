26ca10d3a81060ceccc46e01df053a753f6765fe
JayeeHong <hjy48486@naver.com>
2025-04-06 21:35:52 +0900

    feat: 사용자 조회, 사용자 잔액 충전 추가

--- src/main/java/kr/hhplus/be/server/application/user/UserService.java
+++ src/main/java/kr/hhplus/be/server/application/user/UserService.java
@@ -1,6 +1,8 @@
 package kr.hhplus.be.server.application.user;
 
+import kr.hhplus.be.server.domain.common.Amount;
 import kr.hhplus.be.server.domain.user.User;
+import kr.hhplus.be.server.infrastructure.user.UserRepository;
 import kr.hhplus.be.server.interfaces.user.UserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,29 +11,40 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
 
-    public UserResponse.User chargeBalance(long userId, long amount) {
+    private final UserRepository userRepository;
+
+    public UserResponse.User chargeBalance(long userId, int amount) {
 
         // 사용자 식별자 유효성 체크
-
-        // 사용자 식별자로 사용자의 잔액 조회
+        User findUser = userRepository.findById(userId);
+        if (findUser == null) {
+            throw new IllegalArgumentException("유효하지 않는 사용자입니다.");
+        }
 
         // amount 값 유효성 체크
+        Amount chargeAmount = Amount.of(amount);
         // 1) 0 초과인지
         // 2) 충전 가능한 최대 금액인지
         // 3) 충전 가능한 최소 금액인지
 
-        // 잔액 충전
+        // 4) 충전 후 금액 체크
+        findUser.addBalance(chargeAmount.getValue());
 
-        return new UserResponse.User(1L, "홍길동", 1000);
+        // 잔액 충전
+        User user = userRepository.addBalance(userId, amount);
+
+        return user.translateUser(user);
     }
 
-    public UserResponse.User getBalance(long userId) {
+    public UserResponse.User getUser(long userId) {
 
         // 사용자 식별자 유효성 체크
+        User findUser = userRepository.findById(userId);
+        if (findUser == null) {
+            throw new IllegalArgumentException("유효하지 않는 사용자입니다.");
+        }
 
-        // 사용자 식별자로 사용자의 잔액 조회
-
-        return new UserResponse.User(1L, "홍길동", 1000);
+        return findUser.translateUser(findUser);
     }
 
 }
