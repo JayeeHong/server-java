f40c5e221b15aa442df123814056d954b0e6e287
JayeeHong <hjy48486@naver.com>
2025-04-06 18:03:44 +0900

    fix: 사용자 도메인 Clean + Layered Architecture 적용

---
+++ src/main/java/kr/hhplus/be/server/infrastructure/user/UserRepository.java
@@ -0,0 +1,5 @@
+package kr.hhplus.be.server.infrastructure.user;
+
+public interface UserRepository {
+
+}
---
+++ src/main/java/kr/hhplus/be/server/infrastructure/user/UserRepositoryImpl.java
@@ -0,0 +1,8 @@
+package kr.hhplus.be.server.infrastructure.user;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepositoryImpl implements UserRepository {
+
+}
