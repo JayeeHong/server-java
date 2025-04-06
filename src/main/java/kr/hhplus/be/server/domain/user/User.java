402c449cf48035d50f25905b3c89874c38adc59b
JayeeHong <hjy48486@naver.com>
2025-04-06 21:34:06 +0900

    fix: 사용자 잔액 충전 추가 및 충전 시 유효성 체크 추가

--- src/main/java/kr/hhplus/be/server/domain/user/User.java
+++ src/main/java/kr/hhplus/be/server/domain/user/User.java
@@ -1,11 +1,43 @@
 package kr.hhplus.be.server.domain.user;
 
-import io.swagger.v3.oas.annotations.media.Schema;
+import kr.hhplus.be.server.interfaces.user.UserResponse;
 
 public record User(
     long id,
     String name,
     int balance
 ) {
+
+    private static final long MIN_BALANCE = 0;
+    private static final long MAX_BALANCE = 1000000; //100만원
+
+    public User {
+        if (balance < MIN_BALANCE) {
+            throw new IllegalArgumentException("잔액이 부족합니다.");
+        }
+
+        if (balance > MAX_BALANCE) {
+            throw new IllegalArgumentException("최대 잔액은 1,000,000원입니다.");
+        }
+    }
+
+    public static User empty(long id, String name) {
+        return new User(id, name, 0);
+    }
+
+    public void addBalance(int amount) {
+        int addedBalance = balance + amount;
+        validateBalance(addedBalance);
+    }
+
+    private void validateBalance(int balance) {
+        if (balance > MAX_BALANCE) {
+            throw new IllegalArgumentException("최대 잔액은 1,000,000원입니다.");
+        }
+    }
+
+    public UserResponse.User translateUser(User user) {
+        return new UserResponse.User(user.id(), user.name(), user.balance());
+    }
 
 }
