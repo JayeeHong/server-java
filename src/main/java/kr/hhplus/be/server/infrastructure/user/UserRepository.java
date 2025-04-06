f4c94b6e0e7628ea6a87e8436fbe001c6fc70dc4
JayeeHong <hjy48486@naver.com>
2025-04-06 21:34:49 +0900

    feat: 사용자 조회, 사용자 잔액 충전 추가

--- src/main/java/kr/hhplus/be/server/infrastructure/user/UserRepository.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/user/UserRepository.java
@@ -1,5 +1,11 @@
 package kr.hhplus.be.server.infrastructure.user;
 
+import kr.hhplus.be.server.domain.user.User;
+
 public interface UserRepository {
+
+    User findById(long userId);
+
+    User addBalance(long userId, int amount);
 
 }
--- src/main/java/kr/hhplus/be/server/infrastructure/user/UserRepositoryImpl.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/user/UserRepositoryImpl.java
@@ -1,8 +1,18 @@
 package kr.hhplus.be.server.infrastructure.user;
 
+import kr.hhplus.be.server.domain.user.User;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserRepositoryImpl implements UserRepository {
 
+    @Override
+    public User findById(long userId) {
+        return new User((userId), "userA", 100);
+    }
+
+    @Override
+    public User addBalance(long userId, int amount) {
+        return new User((userId), "userA", amount);
+    }
 }
