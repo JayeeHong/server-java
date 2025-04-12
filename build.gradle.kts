99ba463762e526f0b5e1dbf5c72acdc9a483f9ec
JayeeHong <hjy48486@naver.com>
2025-04-13 03:31:07 +0900

    config: 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ dependencyManagement {
 
 dependencies {
     // Spring
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
