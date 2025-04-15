fe100683aae0120c796d7cfbdcf1185e74ed9b18
JayeeHong <hjy48486@naver.com>
2025-04-16 00:29:52 +0900

    fix: 쿠폰 조회 쿼리 수정

--- src/main/java/kr/hhplus/be/server/infrastructure/coupon/JpaCouponRepository.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/coupon/JpaCouponRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface JpaCouponRepository extends JpaRepository<Coupon, Long> {
 
-    @Query("SELECT c FROM Coupon c WHERE c.quantity > 0 AND c.expiredAt > CURRENT_TIMESTAMP")
+    @Query("SELECT c FROM Coupon c WHERE c.stock > 0 AND c.expiredAt > CURRENT_TIMESTAMP")
     List<Coupon> findAllAvailable();
 }
