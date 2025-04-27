427550364ad483708bd98e789c2e9bd9060d5b8e
JayeeHong <hjy48486@naver.com>
2025-04-27 11:36:07 +0900

    test: pub/sub 기반 락 충돌 테스트

--- src/test/java/kr/hhplus/be/server/config/RedisTest.java
+++ src/test/java/kr/hhplus/be/server/config/RedisTest.java
@@ -2,7 +2,12 @@ package kr.hhplus.be.server.config;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import kr.hhplus.be.server.config.redis.RedissonLockService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
@@ -19,6 +24,56 @@ public class RedisTest {
 
     @Autowired
     private StringRedisTemplate redisTemplate;
+
+    @Autowired
+    private RedissonLockService lockService;
+
+    @Test
+    @DisplayName("Pub/Sub 기반 락 충돌 테스트 - 10개 스레드 중 1개만 성공")
+    void pubSubLockConcurrencyTest() throws InterruptedException {
+
+        // given
+        String key = "pubsub:lock:test";
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        CountDownLatch latch = new CountDownLatch(10);
+
+        AtomicInteger successCount = new AtomicInteger(0);
+        AtomicInteger failCount = new AtomicInteger(0);
+
+        // when
+        for (int i = 0; i < 10; i++) {
+            executorService.submit(() -> {
+                try {
+                    boolean locked = lockService.tryLock(key);
+                    if (locked) {
+                        log.info("락 획득 성공 by " + Thread.currentThread().getName());
+                        successCount.incrementAndGet();
+                        Thread.sleep(2000); //작업하는 것처럼 처리
+                    } else {
+                        log.info("락 획득 실패 by " + Thread.currentThread().getName());
+                        failCount.incrementAndGet();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        lockService.unlock(key);
+                    } catch (Exception ignore) {
+                        log.info("unlock 실패... {}", ignore);
+                    }
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+        executorService.shutdown();
+
+        // then
+        assertThat(successCount.get()).isEqualTo(1); //1개 스레드만 성공
+        assertThat(failCount.get()).isEqualTo(9);
+    }
 
     @Test
     @DisplayName("Redis 연결 및 기본 데이터 저장/조회 테스트")
