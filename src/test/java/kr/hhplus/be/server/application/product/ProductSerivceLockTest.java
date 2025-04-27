c960920f372519bfd864c58ae37d5d9ce0a4272e
JayeeHong <hjy48486@naver.com>
2025-04-27 11:36:50 +0900

    test: 재고 감소 시 Redisson 테스트

--- src/test/java/kr/hhplus/be/server/application/product/ProductSerivceLockTest.java
+++ src/test/java/kr/hhplus/be/server/application/product/ProductSerivceLockTest.java
@@ -1,12 +1,16 @@
 package kr.hhplus.be.server.application.product;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import jakarta.persistence.EntityManager;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import kr.hhplus.be.server.config.redis.RedissonLockService;
+import kr.hhplus.be.server.config.redis.RedissonResultDto;
 import kr.hhplus.be.server.domain.product.Product;
 import kr.hhplus.be.server.domain.product.ProductRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -15,7 +19,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.PlatformTransactionManager;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -29,10 +32,81 @@ public class ProductSerivceLockTest {
     private ProductRepository productRepository;
 
     @Autowired
-    private PlatformTransactionManager transactionManager;
+    private RedissonLockService lockService;
 
-    @Autowired
-    private EntityManager em;
+    @Test
+    @DisplayName("상품 재고 감소 Redisson 테스트 - 성공")
+    void decreaseStockRedissonSuccessTest() {
+
+        // given
+        Product product = Product.create("productA", 1000, 10);
+        productRepository.save(product);
+
+        // when
+        RedissonResultDto result = productService.decreaseStockWithRedisson(product.getId(), 1);
+
+        // then
+        Product updatedProduct = productRepository.findById(product.getId());
+        assertThat(result.isSuccessYn()).isTrue();
+        assertThat(updatedProduct.getStock()).isEqualTo(9);
+    }
+
+    @Test
+    @DisplayName("상품 재고 감소 Redisson 테스트 - 락 획득 실패")
+    void decreaseStockRedissonFailTest() throws InterruptedException {
+
+        // given
+        Product product = Product.create("productA", 1000, 10);
+        productRepository.save(product);
+
+        // 메인 스레드에서 락 먼저 획득
+        boolean locked = lockService.tryLock(String.valueOf(product.getId()));
+        assertThat(locked).isTrue();
+
+        CountDownLatch latch = new CountDownLatch(1);
+        AtomicBoolean tryLockSuccess = new AtomicBoolean(false);
+
+        AtomicReference<RedissonResultDto> result = new AtomicReference<>();
+        
+        // 다른 스레드에서 락 시도하기
+        new Thread(() -> {
+            try {
+                // when
+                result.set(productService.decreaseStockWithRedisson(product.getId(), 1));
+
+                // then
+                tryLockSuccess.set(result.get().isSuccessYn());
+            } finally {
+                latch.countDown();
+            }
+        }).start();
+
+        latch.await();
+        lockService.unlock(String.valueOf(product.getId()));
+
+        // then
+        assertThat(result.get().isSuccessYn()).isFalse();
+        log.info("재고 감소 실패 메세지: {}", result.get().getMessage());
+    }
+    
+    @Test
+    @DisplayName("상품 재고 감소 Redisson 테스트 - 재고 부족 실패")
+    void decreaseStockRedissonFailTest_noStock() {
+
+        // given
+        Product product = Product.create("productA", 1000, 10);
+        productRepository.save(product);
+        
+        // when
+        RedissonResultDto result = productService.decreaseStockWithRedisson(product.getId(), 20);
+        
+        // then 
+        assertThat(result.isSuccessYn()).isFalse();
+        log.info("재고 감소 실패 메세지: {}", result.getMessage());
+
+        Product findProduct = productRepository.findById(product.getId());
+        assertThat(findProduct.getStock()).isEqualTo(10); // 재고 감소 없이 그대로
+    }
 
     @Test
     @DisplayName("상품 재고 감소 비관적락 테스트")
@@ -90,7 +164,7 @@ public class ProductSerivceLockTest {
         for (int i = 0; i < numberOfThreads; i++) {
             executorService.submit(() -> {
                 try {
-                    productService.decreaseStock(product.getId(), 1);
+                    productService.decreaseStockWithOptimisticLock(product.getId(), 1);
                 } catch (Exception e) {
                     // 예외 터지는 갯수 체크
                     catchCount.getAndIncrement();
