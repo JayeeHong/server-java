06be552dca4deab4f8189d103846320092cf319a
JayeeHong <hjy48486@naver.com>
2025-04-29 22:19:19 +0900

    fix: 우선순위 설정

--- src/main/java/kr/hhplus/be/server/config/redis/DistributedLock.java
+++ src/main/java/kr/hhplus/be/server/config/redis/DistributedLock.java
@@ -4,9 +4,11 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.springframework.context.annotation.Primary;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
+@Primary
 public @interface DistributedLock {
     String key(); // 락을 걸 키
     long waitTime() default 10_000L; // 락 대기 시간 (ms)
