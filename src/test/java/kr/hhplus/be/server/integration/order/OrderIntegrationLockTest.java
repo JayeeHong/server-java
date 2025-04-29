ca448ed1d1fb9479aa75215bcd1e6ef3fb1a6041
JayeeHong <hjy48486@naver.com>
2025-04-29 21:32:30 +0900

    test: 주문 파사드 락 테스트

--- src/test/java/kr/hhplus/be/server/integration/order/OrderIntegrationLockTest.java
+++ src/test/java/kr/hhplus/be/server/integration/order/OrderIntegrationLockTest.java
@@ -1,13 +1,18 @@
 package kr.hhplus.be.server.integration.order;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import kr.hhplus.be.server.application.coupon.CouponService;
 import kr.hhplus.be.server.application.order.OrderFacade;
 import kr.hhplus.be.server.application.order.OrderService;
 import kr.hhplus.be.server.application.product.ProductService;
 import kr.hhplus.be.server.application.user.UserService;
+import kr.hhplus.be.server.config.redis.RedissonLockManager;
 import kr.hhplus.be.server.domain.balance.Balance;
 import kr.hhplus.be.server.domain.balance.BalanceRepository;
 import kr.hhplus.be.server.domain.product.Product;
@@ -17,6 +22,7 @@ import kr.hhplus.be.server.domain.user.UserRepository;
 import kr.hhplus.be.server.interfaces.order.OrderRequest.Command;
 import kr.hhplus.be.server.interfaces.order.OrderRequest.Item;
 import kr.hhplus.be.server.interfaces.order.OrderResponse.Result;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +31,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @Transactional
+@Slf4j
 public class OrderIntegrationLockTest {
 
     @Autowired
@@ -41,6 +48,9 @@ public class OrderIntegrationLockTest {
 
     @Autowired
     private CouponService couponService;
+
+    @Autowired
+    private RedissonLockManager lockService;
 
     @Autowired
     private ProductRepository productRepository;
@@ -90,4 +100,49 @@ public class OrderIntegrationLockTest {
         assertThat(userBalance).isEqualTo(980000);
     }
 
+    @Test
+    @DisplayName("락 선점 중 주문 실패 테스트")
+    void placeOrderWithLockFailTest_dueToLock() throws InterruptedException {
+        // given
+        // 1. 상품 등록
+        Product product = Product.create("productA", 10000, 100);
+        productRepository.save(product);
+
+        // 2. 사용자 등록
+        User user = User.create("userA");
+        userRepository.save(user);
+        // 2-1. 사용자 잔액 충전
+        Balance balance = Balance.charge(user.id(), 1000000);
+        balanceRepository.save(balance);
+
+        // 3. 주문 요청 생성
+        List<Item> items = List.of(new Item(product.getId(), 2)); // 상품 2개 주문
+        Command command = Command.of(user.id(), null, items);
+
+        // 락을 미리 잡아놓기 (다른 스레드처럼 시뮬레이션)
+        String lockKey = "order-lock:user:" + user.id() + ":products:" + product.getId();
+        boolean locked = lockService.tryLock(lockKey);
+        assertThat(locked).isTrue();
+
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        CountDownLatch latch = new CountDownLatch(1);
+
+        try {
+            // when
+            executorService.submit(() -> {
+                try {
+                    assertThatThrownBy(() -> orderFacade.placeOrder(command))
+                        .isInstanceOf(IllegalStateException.class)
+                        .hasMessage("현재 주문 처리 중입니다. 잠시 후 다시 시도해주세요.");
+                } finally {
+                    latch.countDown();
+                }
+            });
+
+            latch.await(); // 다른 스레드 완료 대기
+        } finally {
+            lockService.unlock(lockKey);
+            executorService.shutdown();
+        }
+    }
 }
