bbc076786d66a6373e41dd5b4a337ad78d77b4b6
JayeeHong <hjy48486@naver.com>
2025-05-01 23:11:33 +0900

    fix: 사용자 도메인 infrastructure layer

--- src/main/java/kr/hhplus/be/server/infrastructure/user/UserJpaRepository.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/user/UserJpaRepository.java
@@ -3,6 +3,6 @@ package kr.hhplus.be.server.infrastructure.user;
 import kr.hhplus.be.server.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JpaUserRepository extends JpaRepository<User, Long> {
+public interface UserJpaRepository extends JpaRepository<User, Long> {
 
 }
--- src/main/java/kr/hhplus/be/server/infrastructure/user/UserRepositoryImpl.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/user/UserRepositoryImpl.java
@@ -3,27 +3,23 @@ package kr.hhplus.be.server.infrastructure.user;
 import kr.hhplus.be.server.domain.user.User;
 import kr.hhplus.be.server.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 
-@Repository
+@Component
 @RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepository {
 
-    private final JpaUserRepository jpaUserRepository;
+    private final UserJpaRepository userJpaRepository;
 
     @Override
     public User findById(Long userId) {
-        return jpaUserRepository.findById(userId).orElse(null);
-    }
-
-    @Override
-    public User findOrThrow(Long userId) {
-        return jpaUserRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+        return userJpaRepository.findById(userId)
+            .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
     }
 
     @Override
     public User save(User user) {
-        return jpaUserRepository.save(user);
+        return userJpaRepository.save(user);
     }
 
 }
