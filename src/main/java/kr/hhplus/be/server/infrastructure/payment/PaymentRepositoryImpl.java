bc59329b2f464f99923148ce328f47bf9805e6e3
JayeeHong <hjy48486@naver.com>
2025-05-04 09:25:50 +0900

    fix: Payment 도메인 기능 구현

---
+++ src/main/java/kr/hhplus/be/server/infrastructure/payment/PaymentRepositoryImpl.java
@@ -0,0 +1,24 @@
+package kr.hhplus.be.server.infrastructure.payment;
+
+import java.util.Optional;
+import kr.hhplus.be.server.domain.payment.Payment;
+import kr.hhplus.be.server.domain.payment.PaymentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PaymentRepositoryImpl implements PaymentRepository {
+
+    private final PaymentJpaRepository paymentJpaRepository;
+
+    @Override
+    public Payment save(Payment payment) {
+        return paymentJpaRepository.save(payment);
+    }
+
+    @Override
+    public Optional<Payment> findById(Long id) {
+        return paymentJpaRepository.findById(id);
+    }
+}
