79e8df3ffb069a1142d5b7f583390cbe50981687
JayeeHong <hjy48486@naver.com>
2025-05-05 20:32:41 +0900

    fix: BalanceFacade 구현

---
+++ src/main/java/kr/hhplus/be/server/application/balance/BalanceFacade.java
@@ -0,0 +1,29 @@
+package kr.hhplus.be.server.application.balance;
+
+import kr.hhplus.be.server.domain.balance.BalanceInfo;
+import kr.hhplus.be.server.domain.balance.BalanceService;
+import kr.hhplus.be.server.domain.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class BalanceFacade {
+
+    private final UserService userService;
+    private final BalanceService balanceService;
+
+    @Transactional
+    public void chargeBalance(BalanceCriteria.Charge criteria) {
+        userService.getUser(criteria.getUserId());
+        balanceService.chargeBalance(criteria.toCommand());
+    }
+
+    @Transactional(readOnly = true)
+    public BalanceResult.Balance getBalance(Long userId) {
+        userService.getUser(userId);
+        BalanceInfo.Balance balance = balanceService.getBalance(userId);
+        return BalanceResult.Balance.of(balance.getAmount());
+    }
+}
