001eaf2f070572aeffaf27c63bbc0f1959e711e5
JayeeHong <hjy48486@naver.com>
2025-05-05 19:53:31 +0900

    fix: OrderFacade 구현

---
+++ src/main/java/kr/hhplus/be/server/application/order/OrderCriteria.java
@@ -0,0 +1,79 @@
+package kr.hhplus.be.server.application.order;
+
+import java.util.List;
+import kr.hhplus.be.server.domain.balance.BalanceCommand;
+import kr.hhplus.be.server.domain.order.OrderCommand;
+import kr.hhplus.be.server.domain.order.OrderInfo;
+import kr.hhplus.be.server.domain.payment.PaymentCommand;
+import kr.hhplus.be.server.domain.product.ProductCommand;
+import kr.hhplus.be.server.domain.product.ProductCommand.OrderItems;
+import kr.hhplus.be.server.domain.product.ProductInfo;
+import kr.hhplus.be.server.domain.user.UserCouponCommand;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class OrderCriteria {
+
+    @Getter
+    public static class OrderPayment {
+
+        private final Long userId;
+        private final Long couponId;
+        private final List<OrderItem> products;
+
+        private OrderPayment(Long userId, Long couponId, List<OrderItem> products) {
+            this.userId = userId;
+            this.couponId = couponId;
+            this.products = products;
+        }
+
+        public static OrderPayment of(Long userId, Long couponId, List<OrderItem> products) {
+            return new OrderPayment(userId, couponId, products);
+        }
+
+        public ProductCommand.OrderItems toProductCommand() {
+            return OrderItems.of(
+                products.stream()
+                    .map(o -> ProductCommand.OrderItem.of(o.getProductId(), o.getQuantity()))
+                    .toList()
+            );
+        }
+
+        public OrderCommand.Create toOrderCommand(Long userCouponId, int discountAmount, ProductInfo.OrderItems productInfo) {
+            List<OrderCommand.OrderItem> orderProducts = productInfo.getOrderItems().stream()
+                .map(p -> OrderCommand.OrderItem.of(p.getProductId(), p.getProductName(), p.getProductPrice(), p.getQuantity())).toList();
+
+            return OrderCommand.Create.of(userId, userCouponId, discountAmount, orderProducts);
+        }
+
+        public UserCouponCommand.UsableCoupon toCouponCommand() {
+            return UserCouponCommand.UsableCoupon.of(userId, couponId);
+        }
+
+        public BalanceCommand.Use toBalanceCommand(long totalPrice) {
+            return BalanceCommand.Use.of(userId, totalPrice);
+        }
+
+        public PaymentCommand.Payment toPaymentCommand(OrderInfo.Order order) {
+            return PaymentCommand.Payment.of(order.getOrderId(), userId, order.getTotalPrice());
+        }
+    }
+
+    @Getter
+    public static class OrderItem {
+
+        private final Long productId;
+        private final int quantity;
+
+        private OrderItem(Long productId, int quantity) {
+            this.productId = productId;
+            this.quantity = quantity;
+        }
+
+        public static OrderItem of(Long productId, int quantity) {
+            return new OrderItem(productId, quantity);
+        }
+    }
+}
--- src/main/java/kr/hhplus/be/server/application/order/OrderFacade.java
+++ src/main/java/kr/hhplus/be/server/application/order/OrderFacade.java
@@ -1,96 +1,63 @@
 package kr.hhplus.be.server.application.order;
 
+import java.util.Optional;
+import kr.hhplus.be.server.domain.balance.BalanceService;
+import kr.hhplus.be.server.domain.coupon.CouponInfo;
+import kr.hhplus.be.server.domain.coupon.CouponInfo.Coupon;
+import kr.hhplus.be.server.domain.coupon.CouponService;
+import kr.hhplus.be.server.domain.order.OrderCommand.Create;
+import kr.hhplus.be.server.domain.order.OrderInfo.Order;
+import kr.hhplus.be.server.domain.order.OrderService;
+import kr.hhplus.be.server.domain.payment.PaymentService;
+import kr.hhplus.be.server.domain.product.ProductInfo.OrderItems;
+import kr.hhplus.be.server.domain.product.ProductService;
+import kr.hhplus.be.server.domain.user.UserCouponInfo;
+import kr.hhplus.be.server.domain.user.UserCouponInfo.UsableCoupon;
+import kr.hhplus.be.server.domain.user.UserCouponService;
+import kr.hhplus.be.server.domain.user.UserService;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class OrderFacade {
 
-//    private final ProductService productService;
-//
-//    public Result placeOrderWithLock(OrderRequest.Command command) {
-//        // 1. 상품 조회 및 재고 차감
-//        command.getItems().forEach(item ->
-//            productService.decreaseStockWithRedisson(item.getProductId(), item.getQuantity()));
-////        List<Product> products = productService.getAndDecreaseStock(command.getItems());
-//
-////        // 2. 상품 ID -> 상품 매핑
-////        Map<Long, Product> productMap = products.stream()
-////            .collect(Collectors.toMap(Product::getId, p -> p));
-//
-//        // 3. 주문 항목 생성 및 총 금액 계산
-//        List<OrderItem> orderItems = new ArrayList<>();
-//        int totalAmount = 0;
-//
-//        for (OrderRequest.Item item : command.getItems()) {
-//            Product product = productService.getProductInfo(item.getProductId());
-////            Product product = productMap.get(item.getProductId());
-//            if (product == null) {
-//                throw new IllegalArgumentException("존재하지 않는 상품입니다. ID=" + item.getProductId());
-//            }
-//
-//            OrderItem orderItem = OrderItem.create(product.getId(), product.getName(), product.getPrice(), item.getQuantity());
-//            orderItems.add(orderItem);
-////            totalAmount += orderItem.calculateTotalPrice(product.getPrice());
-//        }
-//
-//        // 4. 쿠폰 적용
-//        // TODO 쿠폰 적용
-//        Coupon usedCoupon = null;
-//
-//        // 5. 잔액 차감
-////        userService.validateAndPay(command.getUserId(), totalAmount);
-//
-//        // 6. 주문 생성 및 저장 (연관관계 연결)
-////        Order order = Order.of(null, command.getUserId(), totalAmount);
-//
-////        Order savedOrder = orderService.createOrder(order); // cascade = ALL이면 orderItem도 저장됨
-//
-//        return Result.from(null, orderItems, usedCoupon);
-//    }
-//
-//    @Transactional
-//    public Result placeOrder(OrderRequest.Command command) {
-//        // 1. 상품 조회 및 재고 차감
-//        List<Product> products = productService.getAndDecreaseStock(command.getItems());
-//
-//        // 2. 상품 ID -> 상품 매핑
-//        Map<Long, Product> productMap = products.stream()
-//            .collect(Collectors.toMap(Product::getId, p -> p));
-//
-//        // 3. 주문 항목 생성 및 총 금액 계산
-//        List<OrderItem> orderItems = new ArrayList<>();
-//        int totalAmount = 0;
-//
-//        for (OrderRequest.Item item : command.getItems()) {
-//            Product product = productMap.get(item.getProductId());
-//            if (product == null) {
-//                throw new IllegalArgumentException("존재하지 않는 상품입니다. ID=" + item.getProductId());
-//            }
-//
-//            OrderItem orderItem = OrderItem.create(product.getId(), product.getName(), product.getPrice(), item.getQuantity());
-//            orderItems.add(orderItem);
-////            totalAmount += orderItem.calculateTotalPrice(product.getPrice());
-//        }
-//
-//        // 4. 쿠폰 적용
-//        // TODO 쿠폰 적용
-//        Coupon usedCoupon = null;
-////        if (command.getCouponId() != null) {
-////            usedCoupon = couponService.useCoupon(command.getUserId(), command.getCouponId());
-////            totalAmount = Math.max(0, totalAmount - usedCoupon.getDiscountAmount());
-////        }
-//
-//        // 5. 잔액 차감
-////        userService.validateAndPay(command.getUserId(), totalAmount);
-//
-//        // 6. 주문 생성 및 저장 (연관관계 연결)
-////        Order order = Order.of(null, command.getUserId(), totalAmount);
-//
-////        Order savedOrder = orderService.createOrder(order); // cascade = ALL이면 orderItem도 저장됨
-//
-//        return Result.from(null, orderItems, usedCoupon);
-//    }
+    private final UserService userService;
+    private final ProductService productService;
+    private final UserCouponService userCouponService;
+    private final CouponService couponService;
+    private final OrderService orderService;
+    private final BalanceService balanceService;
+    private final PaymentService paymentService;
+
+    @Transactional
+    public OrderResult.Order orderPayment(OrderCriteria.OrderPayment criteria) {
+
+        userService.getUser(criteria.getUserId());
+
+        OrderItems orderProducts = productService.getOrderProducts(criteria.toProductCommand());
+
+        Optional<Long> couponId = Optional.ofNullable(criteria.getCouponId());
+        Optional<UsableCoupon> usableCoupon = couponId.map(
+            id -> userCouponService.getUsableCoupon(criteria.toCouponCommand()));
+        Optional<Coupon> coupon = couponId.map(couponService::getCoupon);
+
+        Create orderCommand = criteria.toOrderCommand(
+            usableCoupon.map(UsableCoupon::getUserCouponId).orElse(null),
+            coupon.map(Coupon::getDiscountAmount).orElse(0),
+            orderProducts);
+
+        Order order = orderService.createOrder(orderCommand);
+
+        balanceService.useBalance(criteria.toBalanceCommand(order.getTotalPrice()));
+        usableCoupon.ifPresent(c -> userCouponService.useUserCoupon(c.getUserCouponId()));
+        productService.decreaseStock(criteria.toProductCommand());
+        paymentService.pay(criteria.toPaymentCommand(order));
+        orderService.paidOrder(order.getOrderId());
+
+        return OrderResult.Order.of(order);
+    }
+
 }
