5c343a2256d8aafbbc2bae44c731e35402380ad2
JayeeHong <hjy48486@naver.com>
2025-05-06 17:11:49 +0900

    config: Redis 설정

--- src/main/java/kr/hhplus/be/server/config/redis/LockAspect.java
+++ src/main/java/kr/hhplus/be/server/config/redis/LockAspect.java
@@ -1,10 +1,13 @@
 package kr.hhplus.be.server.config.redis;
 
+import java.lang.reflect.Method;
 import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Component;
@@ -12,32 +15,38 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class LockAspect {
 
+    private static final String REDISSON_LOCK_PREFIX = "LOCK:";
+
     private final RedissonClient redissonClient;
+    private final AopForTransaction aopForTransaction;
 
-    @Around("@annotation(distributedLock)")
-    public Object around(
-        ProceedingJoinPoint joinPoint, DistributedLock distributedLock) throws Throwable {
+    @Around("@annotation(kr.hhplus.be.server.config.redis.DistributedLock)")
+    public Object lock(final ProceedingJoinPoint joinPoint) throws Throwable {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        DistributedLock distributedLock = method.getAnnotation(DistributedLock.class);
 
-        String key = distributedLock.key();
-        long waitTime = distributedLock.waitTime();
-        long leaseTime = distributedLock.leaseTime();
+        String key = REDISSON_LOCK_PREFIX + CustomSpringELParser.getDynamicValue(signature.getParameterNames(), joinPoint.getArgs(), distributedLock.key());
+        RLock rLock = redissonClient.getLock(key);  // (1) 락의 이름으로 RLock 인스턴스를 가져온다.
 
-        RLock lock = redissonClient.getLock(key);
-
-        boolean acquired = false;
         try {
-            acquired = lock.tryLock(waitTime, leaseTime, TimeUnit.MILLISECONDS);
-
-            if (!acquired) {
-                throw new IllegalStateException("락 획득 실패: key = {}" + key);
+            // (2) 정의된 waitTime까지 획득을 시도한다, 정의된 leaseTime이 지나면 잠금을 해제한다.
+            boolean available = rLock.tryLock(distributedLock.waitTime(), distributedLock.leaseTime(), distributedLock.timeUnit());
+            if (!available) {
+                return false;
             }
 
-            return joinPoint.proceed();
+            return aopForTransaction.proceed(joinPoint);  // (3) DistributedLock 어노테이션이 선언된 메서드를 별도의 트랜잭션으로 실행한다.
+        } catch (InterruptedException e) {
+            throw new InterruptedException();
         } finally {
-            if (acquired && lock.isHeldByCurrentThread()) {
-                lock.unlock();
+            try {
+                rLock.unlock();   // (4) 종료 시 무조건 락을 해제한다.
+            } catch (IllegalMonitorStateException e) {
+                log.info("Redisson Lock Already UnLock");
             }
         }
     }
--- src/main/java/kr/hhplus/be/server/config/redis/RedisConfig.java
+++ src/main/java/kr/hhplus/be/server/config/redis/RedisConfig.java
@@ -1,6 +1,9 @@
 package kr.hhplus.be.server.config.redis;
 
 import java.time.Duration;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
@@ -26,6 +29,18 @@ public class RedisConfig {
 
     @Value("${spring.data.redis.port}")
     private int port;
+
+    private static final String REDISSON_HOST_PREFIX = "redis://";
+
+    @Bean
+    public RedissonClient redissonClient() {
+        RedissonClient redisson = null;
+        Config config = new Config();
+        config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + host + ":" + port);
+        redisson = Redisson.create(config);
+
+        return redisson;
+    }
 
     // Redis 연결을 위한 Connection 생성
     @Bean
