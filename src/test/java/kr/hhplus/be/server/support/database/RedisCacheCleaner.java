8a0d2bd9ecd026b91ebdbd33cdfc59b1c0e13222
JayeeHong <hjy48486@naver.com>
2025-05-11 16:15:10 +0900

    config: 레디스 캐시 설정 추가

---
+++ src/test/java/kr/hhplus/be/server/support/database/RedisCacheCleaner.java
@@ -0,0 +1,44 @@
+package kr.hhplus.be.server.support.database;
+
+import java.util.Arrays;
+import java.util.Set;
+import kr.hhplus.be.server.config.redis.CacheType;
+import kr.hhplus.be.server.config.redis.Cacheable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+@Component
+@Profile("test")
+public class RedisCacheCleaner {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    public void clean() {
+        Arrays.stream(CacheType.values())
+            .map(Cacheable::cacheName)
+            .map(this::getKeys)
+            .filter(this::isExistKeys)
+            .forEach(this::cleanBy);
+    }
+
+    private Set<String> getKeys(String name) {
+        String keyPattern = getKeyPattern(name);
+        return stringRedisTemplate.keys(keyPattern);
+    }
+
+    private String getKeyPattern(String name) {
+        return name + "*";
+    }
+
+    private boolean isExistKeys(Set<String> keys) {
+        return !CollectionUtils.isEmpty(keys);
+    }
+
+    private Long cleanBy(Set<String> keys) {
+        return stringRedisTemplate.delete(keys);
+    }
+}
