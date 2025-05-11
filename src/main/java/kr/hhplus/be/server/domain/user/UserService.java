f119d940389c292c2af370f30963529b177856d9
JayeeHong <hjy48486@naver.com>
2025-05-11 09:05:20 +0900

    fix: @Transactional 어노테이션 추가

--- src/main/java/kr/hhplus/be/server/domain/user/UserService.java
+++ src/main/java/kr/hhplus/be/server/domain/user/UserService.java
@@ -2,6 +2,7 @@ package kr.hhplus.be.server.domain.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -9,11 +10,13 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    @Transactional(readOnly = true)
     public UserInfo.User getUser(Long userId) {
         User user = userRepository.findById(userId);
         return UserInfo.User.of(user.getId(), user.getName());
     }
 
+    @Transactional
     public UserInfo.User createUser(UserCommand.Create command) {
         User user = User.create(command.getName());
         userRepository.save(user);
